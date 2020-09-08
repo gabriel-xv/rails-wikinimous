01de98cd2d03f5d3f0fcf9b3589186cdd62fec80
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-09-08 16:53:01 +0200

    completed the wiki, some style missing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Destroying  my articles"
+Article.destroy_all
+
+puts "Creating new ones"
+
+10.times do
+  new_article = Article.new(
+    title: Faker::JapaneseMedia::OnePiece.akuma_no_mi,
+    content: Faker::JapaneseMedia::OnePiece.quote
+  )
+  new_article.save!
+end
+
+puts "I'm done"
